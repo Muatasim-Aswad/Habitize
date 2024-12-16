8a7f527e2ad8b2f9e36b35f5e1f3912d521392e0
stasel <2033301+stasel@users.noreply.github.com>
2024-12-16 15:17:26 +0100

    Added new blue background

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -3,9 +3,21 @@ import React from "react";
 import TEST_ID from "./Home.testid";
 
 const Home = () => {
+  const headerStyle = {
+    backgroundColor: "lightblue",
+  }
+
+
+
+
+
+
+
+
+  
   return (
     <div data-testid={TEST_ID.container}>
-      <h1>This is the homepage</h1>
+      <h1 style={headerStyle}>This is the homepage</h1>
       <p>Good luck with the project!</p>
     </div>
   );
