465d5259510e32d0aa1e23548274237b0dfaa0c2
stasel <2033301+stasel@users.noreply.github.com>
2024-12-16 15:19:00 +0100

    Fixed linter

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -5,16 +5,8 @@ import TEST_ID from "./Home.testid";
 const Home = () => {
   const headerStyle = {
     backgroundColor: "lightblue",
-  }
+  };
 
-
-
-
-
-
-
-
-  
   return (
     <div data-testid={TEST_ID.container}>
       <h1 style={headerStyle}>This is the homepage</h1>
