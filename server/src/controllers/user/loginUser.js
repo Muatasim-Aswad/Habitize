7ba8a96e6614615bca654c89da57b6132fdd4208
Tugbanur-f <tnur.yurtsever@gmail.com>
2024-12-21 10:56:45 +0100

    #7/ Signing-backend. Install the necessary packages and updated the loginuser (#22)
    * Install the necessary packages and updated the loginuser
    
    * updated loginuser.js

--- server/src/controllers/user/loginUser.js
+++ server/src/controllers/user/loginUser.js
@@ -1,12 +1,35 @@
 import { User } from "../../models/User.js";
-import hash12 from "../../util/hash12.js";
+import AppError from "../../util/AppError.js";
+import bcrypt from "bcrypt";
 
 const loginUser = async (req, res, next) => {
   try {
-    let { email, password } = req.body.newUser;
-    password = await hash12(password);
+    let { email, password } = req.body.user;
+    // password = await hash12(password);
 
-    User.findOne({ email, password }); //isAuthenticated
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new AppError(
+        401,
+        "Invalid email or password", // Message for the client
+        `User with email ${email} does not exist`, // Message for the backend log
+      );
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      throw new AppError(
+        401,
+        "Invalid email or password",
+        `Incorrect password for user with email ${email}`,
+      );
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Sign-in successful",
+      user: { id: user._id, name: user.name, email: user.email },
+    });
 
     //to do: complete logging logic, if authenticated create a session
   } catch (error) {
