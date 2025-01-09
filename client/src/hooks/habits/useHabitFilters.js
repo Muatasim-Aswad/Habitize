23d26025292aeb728f9b06c6a7b941e546b947fc
Mustafa Sarıtaş <mstfsrts@gmail.com>
2025-01-09 15:15:43 +0100

    bug fixed

--- client/src/hooks/habits/useHabitFilters.js
+++ client/src/hooks/habits/useHabitFilters.js
@@ -39,7 +39,12 @@ export const useHabitFilters = (habits) => {
 
   const filterHabitsByDate = (date) => {
     const dateStr = toDateString(date);
+    const today = toDateString(new Date());
+
     const filtered = habits.map((habit) => {
+      if (dateStr === today) {
+        return habit;
+      }
       const historyEntry = habit.history?.find((h) => h.date === dateStr);
       return {
         ...habit,
